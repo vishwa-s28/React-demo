1b56b3aaa7acfa1c13fde1e1ae6a7b22d82bfa71
vishwa <vishwa.s@sarvadhi.com>
2024-11-25 15:06:18 +0530

    Revert "Update Layout.js"
    This reverts commit a53ee834f59f5787411a76d7310f67bb07a16658.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -9,19 +9,15 @@ const Layout = () => {
           <li>
             <Link to="/">Demo of useState, useEffect and useContext</Link>
           </li>
-
           <li>
             <Link to="/useStateVSuseReducer">useState vs useReducer</Link>
           </li>
-
           <li>
             <Link to="/useRef">UseRef</Link>
           </li>
-
           <li>
             <Link to="/useCallbackVSuseMemo">UseCallbackVSuseMemo</Link>
           </li>
-
           <li>
             <Link to="/customHook">Custom Hook</Link>
           </li>
